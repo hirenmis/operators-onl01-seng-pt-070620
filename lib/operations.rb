a7550603e91a9ac3ef37eb0a3b2c2c822fdf76e7
Hiren Mistry <hirenmst@gmail.com>
2020-07-12 19:23:42 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,8 +11,9 @@ end
 
 def not_safe?(speed)
   
-  speed > 60 ? TRUE : FALSE || speed < 40 ? TRUE : FALSE
-	
+  # speed > 60 ? TRUE : FALSE || speed < 40 ? TRUE : FALSE
+	  speed > 60 ? TRUE : FALSE
+	  speed < 40 ? TRUE : FALSE
 end
 	
 
